56580d52a7a1a07958ea74bc08b20fe2cfec175c
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-10-11 19:46:37 -0300

    Working with data

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -26,7 +26,10 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = appointmentRepository.create(provider, parsedDate);
+  const appointment = appointmentRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json(appointment);
 });
