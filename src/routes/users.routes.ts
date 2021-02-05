4b608edb65e51503883df0aebc13cf1a1dc963bb
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2021-02-05 03:55:36 -0300

    upload files

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 
 import UserCreator from '../services/UserCreator';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -17,5 +23,15 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ message: error.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
