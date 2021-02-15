69d0d002948eaa09e506114d1659ff33c697ecfd
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2021-02-15 07:34:02 -0300

    serving static files

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@ import express from 'express';
 import 'reflect-metadata';
 import './database';
 
+import uploadConfig from './config/upload';
+
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
